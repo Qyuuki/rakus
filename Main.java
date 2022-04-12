9ceb3b333acaab759186691e4a4f7d21264dc3f8
ooshima yuuki <toradosioosima@gmail.com>
2022-04-12 18:41:48 +0900

    second

--- Main.java
+++ Main.java
@@ -4,10 +4,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-       System.out.println("Hello World");
        
        for(int i = 0; i < 20; i++) {
-    	   if(i >= 11) {
+    	   if(i >= 10) {
     		   System.out.println("Hello");
     	   }else {
     		   System.out.println("Hello World");
